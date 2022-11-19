362c90ecd44690485973acd79f6899673a86af52
mmolina06 <molina.moises@gmail.com>
2022-11-18 19:23:47 -0500

    adjutando archivos reto 2

---
+++ myFunction.js
@@ -0,0 +1,17 @@
+function myFunction(a,b){
+
+    let array = false;
+if (b === 'b'){
+    array2=true;
+}
+
+return (array2)
+
+}
+
+
+console.log(myFunction([a, b]));
+
+//myFunction({a:1,b:2,c:3},'b');
+//myFunction({x:'a',y:'b',z:'c'},'a')
+//myFunction({x:'a',y:'b',z:undefined},'z')
