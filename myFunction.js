9b688c4933b14d7b20823b237eed16e4b8cb5f0c
mmolina06 <molina.moises@gmail.com>
2022-11-18 20:00:09 -0500

    todo ok 2

--- myFunction.js
+++ myFunction.js
@@ -1,17 +1,18 @@
 function myFunction(a,b){
 
-    let array = false;
-if (b === 'b'){
-    array2=true;
+const datos = Object.keys(a)
+
+for (let i = 0; i < datos.length;i ++){
+
+    if (datos [i] === b ) {
+        if (a[datos[i]] !== undefined){
+            return true;
+    }
+        }
+        
 }
+return false;}
 
-return (array2)
-
-}
-
-
-console.log(myFunction([a, b]));
-
-//myFunction({a:1,b:2,c:3},'b');
-//myFunction({x:'a',y:'b',z:'c'},'a')
-//myFunction({x:'a',y:'b',z:undefined},'z')
+console.log(myFunction({a:1,b:2,c:3},'b'));
+console.log(myFunction({x:'a',y:'b',z:'c'},'a'));
+console.log(myFunction({x:'a',y:'b',z:undefined},'z'));
